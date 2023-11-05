66721c0f229d5f537842fe224f15a680313177c9
HyeseongMo <121005586+hyeseong-mo@users.noreply.github.com>
2023-11-05 15:04:04 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Paul Mo on 2023/11/05.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
